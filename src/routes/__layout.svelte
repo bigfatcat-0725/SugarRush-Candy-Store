9b68806ef62cc73bfc6e3ed6ca937a660e97249c
bigfatcat <bigfatcat@bigfatcat.local>
2022-02-25 16:20:09 +0900

    commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css'
+</script>
+
+<slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&family=Raleway:wght@600&family=Roboto&display=swap');
+</style>
