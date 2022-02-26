e3e6da6545d2b5763b42291402c30eacf32f6a3c
bigfatcat <bigfatcat@bigfatcat.local>
2022-02-26 19:32:13 +0900

    change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,9 @@
 	import '../app.css'
 </script>
 
-<slot />
+<div>
+	<slot />
+</div>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&family=Raleway:wght@600&family=Roboto&display=swap');
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,7 +1,16 @@
 <script>
+	import { goto } from '$app/navigation'
+
+	import supabase from '$lib/db'
 	import { user } from '$lib/stores'
+
+	const logOut = async () => {
+		let { error } = await supabase.auth.signOut()
+		goto('/')
+	}
 
 	console.log($user)
 </script>
 
 <h4>Welcome {$user?.email ? $user.email : ''}!</h4>
+<button on:click="{logOut}">로그아웃</button>
